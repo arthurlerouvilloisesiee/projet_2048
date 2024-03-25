2c6b6f5fc59bb87425e5def1b7e07cc09baf8138
LucienLaumont <150805439+LucienLaumont@users.noreply.github.com>
2024-03-25 16:00:22 +0100

    Update game_ai.py

--- game_ai.py
+++ game_ai.py
@@ -46,46 +46,4 @@ def ai_move(board, searches_per_move, search_length):
     search_board, game_valid, score = best_move(board)
     return search_board, game_valid
 
-def ai_play(board):
-    move_number = 0
-    valid_game = True
-    while valid_game:
-        move_number += 1
-        number_of_simulations, search_length = get_search_params(move_number)
-        board, valid_game = ai_move(board, number_of_simulations, search_length)
-        if valid_game:
-            board = add_two(board)
-        if check_for_win(board):
-            valid_game = False
-        print(board)
-        print(move_number)
-    print(board)
-    return np.amax(board)
-
-def ai_plot(move_func):
-    tick_locations = np.arange(1, 12)
-    final_scores = []
-    for _ in range(SAMPLE_COUNT):
-        print('thing is ', _)
-        board = initialize_game()
-        game_is_win = ai(board)
-        final_scores.append(game_is_win)
-    all_counts = np.zeros(11)
-    unique, counts = np.unique(np.array(final_scores), return_counts=True)
-    unique = np.log2(unique).astype(int)
-    index = 0
-
-    for tick in tick_locations:
-        if tick in unique:
-            all_counts[tick-1] = counts[index]
-            index += 1
-
-    plt.bar(tick_locations, all_counts)
-    plt.xticks(tick_locations, np.power(2, tick_locations))
-    plt.xlabel("Score of Game", fontsize = 24)
-    plt.ylabel(f"Frequency per {SAMPLE_COUNT} runs", fontsize = 24)
-    plt.show()
-
-
-
 	
